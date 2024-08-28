d90f85997e4a2d9a7a27d1d9441e50514e3b080e
Herbert Souto <herbert-93@hotmail.com>
2024-08-28 18:22:06 -0300

    update

--- pipeline_olympics/models/intermediate/postgres/int_postgres_athletes.sql
+++ pipeline_olympics/models/intermediate/postgres/int_postgres_athletes.sql
@@ -1,3 +1,7 @@
+{{ config(
+    schema='intermediate'
+) }}
+
 WITH separated_columns_athletes AS (
     SELECT
         code,
@@ -7,13 +11,28 @@ WITH separated_columns_athletes AS (
         country_full,
         height,
         weight,
-        disciplines,
+        -- Remove colchetes e converte a string para um formato JSON para fácil manipulação
+        trim(both '[]' from disciplines) AS disciplines,
         birth_date
-    FROM {{ ref('stg_postgres_athletes') }}
+    FROM 
+        {{ ref('stg_postgres_athletes') }}
+),
+
+-- Concatena as disciplinas em uma única linha para cada atleta
+concatenated_disciplines AS (
+    SELECT
+        code,
+        name_tv,
+        gender,
+        country_code,
+        country_full,
+        height,
+        weight,
+        -- Concatena disciplinas com delimitador, pode ajustar o delimitador se necessário
+        array_to_string(string_to_array(trim(both '[]' from disciplines), ', '), ', ') AS disciplines,
+        birth_date
+    FROM 
+        separated_columns_athletes
 )
 
-SELECT * FROM separated_columns_athletes
-
-{{ config(
-    schema='intermediate'
-) }}
+SELECT * FROM concatenated_disciplines
